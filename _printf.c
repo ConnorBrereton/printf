9c2d4c726aadd25db85ea2fa5bbe31b95f6b158e
ConnorBrereton <breretonconnor@gmail.com>
2018-10-23 06:15:58 +0000

    The _printf file starts listening on the input stream and handles FORMATTER/SPECIFIER iteration logic. Additionally it returns the length of the characters printed

--- _printf.c
+++ _printf.c
@@ -1,28 +1,56 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
- * struct types - holds the argument type
- * and pointer to function with argument
- * type (our list of parameters).
- *
- * @type: ptr to the char identifier
- *
- * @f: pointer to function of params
- * in the ellipse.
- */
+  * _printf - handles formatter 
+  * and specifier iteration
+  *
+  * @format: va_list declaration
+  * Return: len (success) 1 (fail)
+  */
 
 int _printf(const char *format, ...)
 {
-	int i;
+	int i = 0, len = 0;
+
 	va_list params;
-	
-	type_s controller[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'d', print_decimal},
-		{NULL, NULL}
-	};
+
+	if (format == NULL)
+		return (1);
 
 	va_start(params, format);
+
+	while (format[i] != '\0')
+	{
+		/* case 1: formatter without specifier */
+		if (format[i] == '%' && (format[i + 1] == ' ' || format[i + 1] == '\0'))
+			return (1);
+
+		/* case 2: double formatter */
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			len++;
+			i++;
+		}
+
+		/* case 3: formatter with valid specifier */
+		else if (format[i] == '%' && format[i + 1] != '\0' && format[i + 1] != '%')
+		{
+			len += getter(format, params, i);
+			i++;
+		}
+
+		/* case 4: memory only has formatter */
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+	}
+	/* len = number of characters */
+	return (len);
+}
