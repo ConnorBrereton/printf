58b95f23befb14a627cc2a3732e389107ced0cf6
cchan88 <398@holbertonschool.com>
2018-10-22 20:27:47 -0700

    testing files to merge into different brach

--- printf.c
+++ printf.c
@@ -1,102 +1,34 @@
 #include "holberton.h"
-#include <stdarg.h>
 #include <unistd.h>
-
+#include <stdio.h>
 /**
- * print_char - prints out the
- * char from the buffer.
- *
- * @params: input stream
- *
- * Return: 0 (success)
  */
-
-int print_char(va_list params)
+int print_char(va_list args)
 {
-	/* buffer contains character and \0 */
-	char *buffer[1];
-
-	buffer[0] = va_arg(params, int);
-
-	write(1, buffer, sizeof(char));
-
-	return (0);
+	return (_putchar(va_arg(args, int)));
 }
 
-/**
- * print_1char - prints one char
- *
- * @s: points to the first char
- *
- * Return: 0 (success)
- */
-
-int print_1char(const char *s)
+int print_perc(va_list args)
 {
-	write(1, s, sizeof(char));
+	(void)args;
+	_putchar('%');
+	return(0);
 }
 
-/**
- * print_string - prints one char
- *
- * @params: input stream
- *
- * Return: buffer_len (success) 1 (failure)
- */
-
-int print_string(va_list params)
+int print_str(va_list args)
 {
-	int i, len = 0, buffer_len = 0, count = 0;
-	char *buffer;
-	char null[5] = "NULL";
+	char *str = va_arg(args, char*);
 
-	buffer = va_arg(params, char *);
-	buffer_len = _intlen(buffer);
-	count = sizeof(char) * buffer_len;
+	int i;
 
-	if (!buffer)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		buffer = malloc(sizeof(char) * 5);
-
-		if (buffer == NULL)
-		{
-			free(buffer);
-			return (1);
-		}
-
-		while (null[i] != '\0')
-		{
-			*(buffer + i) = null[i];
-			i++;
-		}
+		_putchar(str[i]);
 	}
 
-	write(1, buffer, count);
-	return (buffer_len);
-}
-
-/**
- * print_decimal - prints decimal stream
- *
- * @params: input stream
- *
- * Return: buffer_len (success) 1 (failure)
- */
-
-int print_decimal(va_list params)
-{
-	int buffer_len = 0, count = 0;
-	int stream;
-	char *buffer;
-
-	stream = va_arg(params, int);
-
-	if (stream < 0)
-		return (1);
-
-	buffer_len = _intlen(stream);
-	buffer = stream;
-	count = sizeof(int) * buffer_len;
-
-	write(1, buffer, count);
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+	return (i);
 }
